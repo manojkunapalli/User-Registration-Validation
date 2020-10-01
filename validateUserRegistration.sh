a5f706d5fa251a37d9f467d10aaccb6da4bdeae0
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 02:41:06 +0530

    Added: mobile Number is validated with Rule 1

--- validateUserRegistration.sh
+++ validateUserRegistration.sh
@@ -5,6 +5,7 @@ firstNamePattern="^[A-Z]{3,}"
 lastNamePattern="^[A-Z]{3,}"
 mailPattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 mobilePattern="^[0-9]{2}[ ]{1}[1-9]{1}[0-9]{9}$"
+passwordPattern="^[a-zA-z]{8,}"
 
 read -p "enter First Name = " firstName
 echo -e
@@ -13,6 +14,9 @@ echo -e
 read -p "enter mail id = " email
 echo -e
 read -p "enter mobile number = " mobile
+echo -e
+read -p "enter password = " password
+
 
 if [[ $firstName =~ $firstNamePattern ]]
 then
@@ -40,4 +44,11 @@ then
 	echo "mobile number is valid"
 else
 	echo "mobile number is not valid"
+fi
+
+if [[ $password =~ $passwordPattern ]]
+then
+	echo "Password is valid"
+else
+	echo "Password is not valid"
 fi
