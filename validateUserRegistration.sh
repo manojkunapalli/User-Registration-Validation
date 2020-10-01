ad0403c9ba40e446baa8652817eb15192b0a1b30
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 02:02:18 +0530

    Added: validated first name

--- validateUserRegistration.sh
+++ validateUserRegistration.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "welcome to User Registration system"
+
+firstNamePattern="^[A-Z]{3,}"
+
+read -p "enter FirstName = " firstName
+
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "First Name is valid"
+else
+	echo "First Name is not valid"
+fi
