af3c8c0623d1ea117e8f2d19f941b381f3ee60c9
manojkunapalli <manojkunapalli@gmail.com>
2020-10-02 02:08:41 +0530

    Added: last name validated

--- validateUserRegistration.sh
+++ validateUserRegistration.sh
@@ -2,12 +2,22 @@
 echo "welcome to User Registration system"
 
 firstNamePattern="^[A-Z]{3,}"
+lastNamePattern="^[A-Z]{3,}"
 
-read -p "enter FirstName = " firstName
+read -p "enter First Name = " firstName
+echo -e
+read -p "enter Last Name = " lastName
 
 if [[ $firstName =~ $firstNamePattern ]]
 then
 	echo "First Name is valid"
 else
 	echo "First Name is not valid"
+fi
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+	echo "Last Name is valid"
+else
+	echo "Last Name is not valid"
 fi
